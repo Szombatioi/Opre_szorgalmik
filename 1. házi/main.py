b210a4db6ef56c882d1dd3db3ff801e05475bab2
Szombatioi <o.szombati@gmail.com>
2023-04-11 20:43:06 +0200

    hf1_20230411

---
+++ 1. házi/main.py
@@ -0,0 +1,13 @@
+class Task:
+    def __init__(self, name: str, priority: int, arrival: int, length: int) -> None:
+        self.name = name
+        self.priority = priority
+        self.arrival = arrival
+        self.length = length
+
+class Scheduler:
+    pass
+
+
+if __name__ == "__main__":
+    print("Hello WOrld")
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-
+BME VIK mérnökinformatika, IV. félév Operációs Rendszerek szorgalmik
+1. Házi:
+    Többszint? ütemez? tervezése
