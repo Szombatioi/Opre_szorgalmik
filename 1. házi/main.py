30fb4281146f8225877d72b6e648055fd60acb1d
Szombatioi <o.szombati@gmail.com>
2023-04-13 22:46:06 +0200

    hf1_20230413

--- 1. házi/main.py
+++ 1. házi/main.py
@@ -4,18 +4,19 @@ class Task: #taszk osztály
         self.prior = prior          #taszk prioritása
         self.arrival = arrival      #taszk indítási ideje
         self.length = length        #taszk CPU-löketideje
-        self.remTime = length       #taszk futásának hátralévő ideje
-        self.wait = 0
+        self.remTime = length       #taszk futásának hátralévő ideje TODO: lehet nem kell?
+        self.waitTime = 0
         self.tickTime = 0           #taszk futásának eltelt ideje
     def wait(self) -> None:
-        self.wait+=1
+        self.waitTime+=1
     def tick(self) -> None:
-        self.length -= 1
+        self.remTime -= 1
+        self.tickTime -= 1
     
     
     #test?
     def __str__(self) -> str:
-        return f"{self.name},{self.prior},{self.length},{self.remTime}"
+        return f"{self.name},{self.prior},{self.arrival},{self.remTime}"
 
 class FIFO: #FIFO osztály az RR ütemezőhöz
     def __init__(self) -> None:
@@ -24,6 +25,7 @@ class FIFO: #FIFO osztály az RR ütemezőhöz
         
     def push(self, elem: Task) -> None: #FIFO végére szúr egy új taszkot
         self.Storage.append(elem) #fordított sorrend lesz, mert ez egy tömb
+        elem.wait()
         self.empty = False
 
     def pop(self) -> Task: #FIFO elejéről kiveszi az ott lévő taszkot
@@ -31,14 +33,20 @@ class FIFO: #FIFO osztály az RR ütemezőhöz
         if len(self.Storage) == 0:
             self.empty = True
         return res
+    def wait(self):
+        for t in self.Storage:
+            t.wait()
+    def top(self) -> Task:
+        return self.Storage[0]
     
 class List: #SRTF ütemezőhöz Lista
     def __init__(self) -> None:
         self.Storage = [] #egyszerű implementáció, ezért csak egy tömb
         self.empty = True #üres-e
 
-    def insert(self, elem: Task) -> None:   #beszúrja a megfelelő helyre az új taszkot
+    def push(self, elem: Task) -> None:   #beszúrja a megfelelő helyre az új taszkot
         index = 0
+        
         for i in range(len(self.Storage)):
             if self.Storage[i].remTime < elem.remTime:
                 index = i
@@ -51,6 +59,11 @@ class List: #SRTF ütemezőhöz Lista
         if len(self.Storage) == 0:
             self.empty = True
         return res
+    def wait(self):
+        for t in self.Storage:
+            t.wait()
+    def top(self) -> Task:
+        return self.Storage[len(self.Storage)-1]
 
 class RR_Scheduler: #RR ütemező az egyik szinthez
     def __init__(self, prior: int, time: int) -> None:
@@ -67,29 +80,31 @@ class RR_Scheduler: #RR ütemező az egyik szinthez
     # def getTask() -> Task: #visszaad egy taszkot a FIFO ele
     #     pass
     
-
 class SRTF_Scheduler: #SRTF ütemező a másik szinthez
     def __init__(self, prior: int) -> None:
         self.prior = prior
         self.List = List()
     def addTask(self, task: Task) -> None: #taszk felvétele az SRTF_Scheduler (rendezett) listájába
-        self.List.insert(task)
+        self.List.push(task)
     def popTask(self) -> Task: #taszk kiválasztása a lista elejéről
         return self.List.pop()
     def tick(self): #TODO: ha ...
         pass
-
-class Scheduler: #többszintű ütemező osztály
+        
+class Scheduler:
     def __init__(self) -> None:
         self.RR_Scheduler = RR_Scheduler(1, 2)
         self.SRTF_Scheduler = SRTF_Scheduler(0)
+        self.finishedTasks = []
         self.activeTask = None
-        self.taskHistory : Task = [] #TODO: ez valid deklaráció Task type-ra?
+        self.text = ""
+        self.temp = [] #átmeneti mappa a még meg nem érkezett taszkoknak
+        
     def addTask(self, task: Task) -> None: #taszk felvétele a megfelelő szintre
-        if task.prior == 0:
+        if task.prior == 1:
             self.RR_Scheduler.addTask(task)
         else:
-            self.SRTF_Scheduler.addTask(task)   
+            self.SRTF_Scheduler.addTask(task) 
     def getInput(self) -> None:
         while True:
             inp = input().strip()
@@ -98,59 +113,84 @@ class Scheduler: #többszintű ütemező osztály
             inp = inp.split(',')
 
             t = Task(inp[0], int(inp[1]), int(inp[2]), int(inp[3]))
-            self.addTask(t)
-            self.taskHistory.append(t)
-
-    def start(self) -> None: #a beadott taszkok után az elindítással kezdődik meg a szimuláció
+            #self.addTask(t)
+            self.temp.append(t)
+            print("Added: ",t)
+            
+    def work(self):
+        time = 0
+        for i in range(len(self.temp)):
         
-        while not SRTF_Scheduler.List.empty and not RR_Scheduler.FIFO.empty: #amíg van taszk valamelyik szinten
-            #choose task
-            ##self.activeTask = self.chooseTask()
-            if self.activeTask is None: #ha nincs aktív taszkunk
-                #choose new task
-                    #from 1 or 0
-                pass
-            if self.activeTask.prior != 1 and not self.SRTF_Scheduler.List.empty: #ha van magas prioritású taszk a Listában
-                self.RR_Scheduler.FIFO.push(self.activeTask) #visszakerül a FIFO végére
-                self.activeTask = self.SRTF_Scheduler.List.pop()
+            print("Is it? ",self.temp[i].arrival)
+            if self.temp[i].arrival <= time:
+                self.addTask(self.temp[i])
+                self.temp.pop(i)
+                i+=1
+        while (not self.RR_Scheduler.FIFO.empty) and (not self.SRTF_Scheduler.List.empty): #itt egy iteráció = egy taszk kiválasztása
+
+            """for task in self.temp:
+                print("Is it? ",task.arrival)
+                if task.arrival <= time:
+                    self.addTask(task)
+                    self.temp.remove(task)
+                    print("removed")"""
+            
+            ###################################
+            # choose task -> prior 1, aztán 0 #
+            ###################################
+            
+            if self.activeTask is not None:
+                if self.activeTask.prior == 0: #SRTF-ből van a taszk
+                    if not self.RR_Scheduler.FIFO.empty and self.RR_Scheduler.FIFO.top().arrival <= time:
+                        self.SRTF_Scheduler.List.push(self.activeTask)
+                        self.activeTask = self.RR_Scheduler.FIFO.pop()
+                    elif self.SRTF_Scheduler.List.top().arrival <= time and self.SRTF_Scheduler.List.top().remTime < self.activeTask.remTime:
+                        self.SRTF_Scheduler.List.push(self.activeTask)
+                        self.activeTask = self.SRTF_Scheduler.List.pop()
+                else: #self.activeTask.prior==1
+                    if self.RR_Scheduler.FIFO.top().remTime < self.activeTask.remTime:
+                        self.RR_Scheduler.FIFO.push(self.activeTask)
+                        self.activeTask = self.RR_Scheduler.FIFO.pop()
             else:
-                #if nem járt le az időszelet a mostaniról
-                if self.activeTask.tickTime < 2:
+                if not self.RR_Scheduler.FIFO.empty and self.RR_Scheduler.FIFO.top().arrival <= time:
                     self.activeTask = self.RR_Scheduler.FIFO.pop()
+                elif self.SRTF_Scheduler.List.top().arrival <= time:
+                    self.activeTask = self.SRTF_Scheduler.List.pop()
+            print("Task: ",self.activeTask)
             self.activeTask.tick()
-            #print a betűjelet
+            if len(self.text) == 0 or (self.text[len(self.text)-1] != self.activeTask.name):
+                self.text += self.activeTask.name
 
-            pass
-        for task in self.taskHistory:
-            print(f"{task.name}:{task.wait},", end="")
+            #minden másik task várakozik
+            self.RR_Scheduler.FIFO.wait()
+            self.SRTF_Scheduler.List.wait()
+
+            if self.activeTask.remTime == 0:
+                print(f"---Task_{self.activeTask.name} is finished")
+                self.finishedTasks.append(self.activeTask)
+                self.activeTask = None
+                continue
+
+            if self.activeTask.prior == 0 and self.activeTask.tickTime == 0: #ha lejár az időszelet, mindenképp kerüljön vissza
+                print("---Time is over")
+                self.RR_Scheduler.FIFO.push(self.activeTask)
+                self.activeTask = None
+            
+            time += 1
+            
+            
+        print("out1:", self.text, "\nout2:")
+        for t in self.finishedTasks:
+            print(f"{t.name}:{t.waitTime}", end=",")
         print()
-        pass
-    def chooseTask(self) -> Task: #kiválasztja a soron következő taszkot és azt kiveszi a tárolójából
-        pass
 
-    def tick(self) -> None: #egy CPU tick -> az aktuális taszk hátralévő ideje csökken
-        #get best task
-        #tick task
-        task = self.chooseTask()
-        if task is None:
-            return
-        
-        #TODO: kell magic
-        task.tick()
-
-        #amely taszkok nem futnak, azok várakozási ideje nő
-        for t in self.RR_Scheduler.FIFO.Storage:
-            t.wait()
-        for t in self.SRTF_Scheduler.List.Storage:
-            t.wait()
-        
-    
 
 if __name__ == "__main__":
-    scheduler = Scheduler()
-    #scheduler.addTask(Task('A', 1,2,3))
-    scheduler.getInput()
-    scheduler.start()
+     scheduler = Scheduler()
+     scheduler.getInput()
+     print("Start:")
+     scheduler.work()
+    
     
     
     
--- 1. házi/tests.py
+++ 1. házi/tests.py
@@ -1,2 +1,4 @@
-arr:str = []
+arr = [1,2,3]
 
+for i in range(len(arr)):
+    print(i)
