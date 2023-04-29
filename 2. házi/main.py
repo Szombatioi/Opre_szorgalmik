f4a5b4d97121af94071f7ba333e1fc68d24bf731
Szombatioi <o.szombati@gmail.com>
2023-04-29 17:23:38 +0200

    hf2_final

--- 2. házi/main.py
+++ 2. házi/main.py
@@ -1,4 +1,3 @@
-test = False
 def index(arr, elem) -> int:
     for i in range(len(arr)):
         if arr[i] == elem:
@@ -11,13 +10,6 @@ class Page:
         self.freeze = 0
         self.frozen = False
         self.referenced = False
-        
-        
-    def __str__(self) -> str:
-        string = f"{self.name}{self.value}({self.freeze})"
-        if self.referenced:
-            string += "'"
-        return string
 
 class Fifo:
     def __init__(self) :
@@ -31,12 +23,6 @@ class Fifo:
     def Contains(self, value: int) -> bool:
         for x in self.storage:
             if x.value == value:
-                if test:
-                    print(f"\n\t{value} -> FIFO: | {self.Get(2)} | {self.Get(1)} | {self.Get(0)}\t{x.name}'\t", end="")
-                # temp = self.storage.pop(index(self.storage, x))
-                # temp.frozen = True
-                # temp.freeze = 3
-                # self.Push(temp)
                 x.referenced = True
                 return True
         return False
@@ -56,50 +42,22 @@ class Fifo:
                 x.freeze -= 1
             if x.freeze == 0:
                 x.frozen = False
-    def PushBack(self, p: Page) :
-        temp = self.storage.pop(index(self.storage, p))
-        self.Push(temp)
-    def Unlock(self, ref: int) : #első hivatkozás után feloldja a keretet
-        for p in self.storage:
-            if p.value == ref:
-                p.frozen = False
-                p.freeze = 0
-    def top(self) -> Page:
-        return self.storage[len(self.storage)-1]
-    
-    def Get(self, n: int): #TEST
-        return self.storage[n]
-    
     def GetFree(self) -> Page:
         i = 0
         while i < (len(self.storage)):
-            if test:
-                print(f"\n\t\ti={i}, Actual: {self.storage[i]}", end="")
             if self.storage[i].freeze > 0:
-                if test:
-                    print(f"\n\t\tSkipped {self.storage[i]}", end="")
                 i+=1
                 continue
-                #self.Push(self.storage.pop(i)) #NEM JÓ A ROTÁLÁS
             
             if self.storage[i].referenced:
-                if test:
-                    print(f"\n\t  -> FIFO: | {fifo.Get(2)} | {fifo.Get(1)} | {fifo.Get(0)}\t{self.storage[i]}!!\t", end="")
                 self.storage[i].referenced = False
-                #self.Decrease()
                 self.Push(self.storage.pop(i))
-                if test:
-                    print(f"\n\t\t  -> FIFO: | {fifo.Get(2)} | {fifo.Get(1)} | {fifo.Get(0)}\t", end="")
-                #i-=1
             else:
                 return self.storage.pop(i)
-            #i+=1
 
 
 if __name__ == "__main__":
-    #line = input().split(',') #1,2,3,-1,5,-1
-    line = input()
-    line = line.split(',')
+    line = input().split(',')
     
     pFaults = 0
     fifo = Fifo()
@@ -112,56 +70,22 @@ if __name__ == "__main__":
     while i < len(line): #minden hivatkozásra
         ref = abs(int(line[i]))
         if fifo.Contains(ref):
-            print("-", end="" if not test else "\n")
+            print("-", end="")
             fifo.Decrease()
-            # temp = fifo.Pop()
-            # fifo.Push(temp)
             
         else:
             if not fifo.HasFree() and not fifo.HasReference(): #ha egyáltalán nincs szabad keret a fifoban
-                print("*", end="" if not test else "\n")
+                print("*", end="")
                 pFaults += 1
                 fifo.Decrease()
             else:
-                # page = fifo.Pop()
-                # while page.freeze > 0: #azaz a kivett lap még fagyasztott #TODO: lehet ez nem jó.  Lehet úgy kell, hogy a fifo elején lévő (fagyasztott) keret maradjon ott, de csökkenjen, a szabadot meg kivesszük
-                #     fifo.Push(page)
-                #     page = fifo.Pop()
-                #     fifo.Decrease()
-                # if page.freeze > 0:
-                #     page = fifo.GetNotFrozen()
-                
                 page = fifo.GetFree()
-
                 page.freeze = 3 #visszaállítjuk
                 page.value = ref
                 fifo.Decrease() #mindent csökkentünk a fifo-ban, de ezt még nem!
                 fifo.Push(page)
                 pFaults += 1
-                print(page.name, end="" if not test else "\n")
+                print(page.name, end="")
                     
         i+=1
     print(f"\n{pFaults}")
-    
-
-
-# done = False
-# while not done:
-#     print("?")
-#     page = fifo.top()
-#     if not page.referenced and not page.freeze>0:
-#         done = True
-#         break
-#     if page.referenced:
-#         page.referenced = False
-#         fifo.Push(fifo.Pop()) #de nincs Decrease!
-#         page = fifo.top()
-#     if page.freeze > 0:
-#         #page.freeze -= 1
-#         #fifo.Push(fifo.Pop()) #TODO: ide kell a magic!
-#         for i in range(len(fifo.storage)-1):
-#             if fifo.storage[i].freeze == 0:
-#                 page = fifo.storage.pop(i)
-#                 fifo.Decrease()
-#                 done = True
-#                 break
